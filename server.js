6c2a0ffed6a9e142d9ad7469da5c8499e39f8669
Igor <igormartins.all@gmail.com>
2024-10-29 08:28:18 -0300

    using foreach with ejs

--- server.js
+++ server.js
@@ -5,7 +5,19 @@ app.set('view engine', 'ejs')
 app.use(express.static('./'))
 
 app.get('/', function(req, res) {
-  return res.render('pages/index')
+  const summary = {
+    title: 'Tópicos essenciais:',
+    items: [
+      'Instalação e Configuração;',
+      'Sintaxe Básica;',
+      'Estruturas de Controle;',
+      'Partials;',
+      'Renderização de Dados Dinâmicos.'
+    ]
+  }
+
+
+  return res.render('pages/index', { summary })
 })
 
 app.get('/about', function(req, res) {
