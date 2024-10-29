3ba197fa1ae23d8184641df2ff752bbd9e36b947
Igor <igormartins.all@gmail.com>
2024-10-28 22:19:42 -0300

    new page and menu

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use(express.static('./'))
 
 app.get('/', function(req, res) {
   return res.render('index')
+})
+
+app.get('/about', function(req, res) {
+  return res.render('about')
 })
 
 app.listen(8080, function() {
